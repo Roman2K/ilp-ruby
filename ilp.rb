806a5f52d7b24ff8474ee36428815e36e0446cc3
Roman Le Négrate <roman.lenegrate@gmail.com>
2021-05-29 00:58:45 +0200

    initial commit

---
+++ ilp.rb
@@ -0,0 +1,156 @@
+require 'uri'
+require 'socket'
+
+class ILP
+  def self.connect(uri)
+    uri = URI uri
+    uri.scheme == 'tcp' or raise "unhandled protocol"
+
+    db = uri.path&.sub %r{^/}, ""
+    db !~ %r{/} or raise "nested paths not supported"
+
+    opts = {}
+    query = Hash[URI.decode_www_form uri.query || ""]
+    query.delete("time_prec")&.then { opts[:time_prec] = _1 }
+    query.empty? or raise "unhandled query params: #{query.keys * ", "}"
+
+    io = TCPSocket.new uri.host, uri.port
+    new io, db, **opts
+  end
+
+  def initialize(io, db, time_prec:)
+    @io = io
+    @db = db
+    @point_class = Class.new(Point).tap do |cls|
+      cls.time_conv = TimeConv.new time_prec
+    end
+  end
+
+  def time_conv = @point_class.time_conv
+
+  def write_points(points)
+    now = nil
+    lines = ""
+    points.each do |attrs|
+      pt = @point_class.new **attrs
+      pt.timestamp ||= now ||= time_conv.now
+      lines << "#{pt}\n"
+    end
+    begin
+      @io.write lines
+    rescue SystemCallError
+      raise WriteError
+    end
+  end
+
+  class Error < StandardError; end
+  class WriteError < Error; end
+  class EmptyValuesError < Error; end
+
+  class Point
+    def initialize(series:, tags:, values:, timestamp: nil)
+      @series, @tags, @values, @timestamp = series, tags, values, timestamp
+    end
+
+    attr_reader :series, :tags, :values
+    attr_accessor :timestamp
+  
+    def to_s
+      s = "#{Esc.esc @series, Esc::MEASUREMENT}"
+      @tags.each do |k,v|
+        v = v.to_s
+        next if v.empty?
+        s << ",#{Esc.esc k, Esc::TAG_KEY}=#{Esc.esc v, Esc::TAG_VALUE}"
+      end
+      s << " " << @values.
+        tap { raise EmptyValuesError if _1.empty? }.
+        map { |k,v| "#{Esc.esc k, Esc::FIELD_KEY}=#{Esc.field_value v}" }.
+        join(",")
+      if @timestamp
+        s << " " << self.class.time_conv.coerce(@timestamp).to_i.to_s
+      end
+      s
+    end
+    
+    class << self
+      attr_writer :time_conv
+      def time_conv; @time_conv or raise "time_conv not set" end
+    end
+
+    module Esc
+      MEASUREMENT = /[ ,]/
+      # https://questdb.io/docs/guides/influxdb-line-protocol/#naming-restrictions
+      TAG_KEY = FIELD_KEY = %r{[ \.\?,:\\/\0\)\(_\+\*~%=]}
+      TAG_VALUE = /[ ,=]/
+      FIELD_VALUE = /[\\"]/
+
+      # https://docs.influxdata.com/influxdb/v1.7/write_protocols/line_protocol_reference/#data-types
+      def self.field_value(v)
+        case v
+        when Integer then "#{v}i"
+        when String then %{"#{esc v, FIELD_VALUE}"}
+        else v.to_s
+        end
+      end
+
+      def self.esc(s, re)
+        s.to_s.gsub(re, "\\\\\\&")
+      end
+    end
+  end
+
+  class TimeConv
+    def initialize(precision)
+      @factor = FACTORS.fetch(precision) do
+        raise ArgumentError, "invalid time precision"
+      end
+      @clock = Clock.new *CLOCK_NAMES.fetch(precision)
+    end
+
+    def now = @clock.get
+    def conv(time)= Timestamp.new(time.to_r * @factor)
+    
+    def coerce(val)
+      case val
+      when Timestamp then val
+      when Time then conv val
+      else raise TypeError, "cannot coerce #{val.class}"
+      end
+    end
+
+    Clock = Struct.new :name, :divisor do
+      def get
+        time = Process.clock_gettime Process::CLOCK_REALTIME, name
+        Timestamp.new time / divisor
+      end
+    end
+
+    Timestamp = Struct.new :to_i do
+      def initialize(val)
+        super val.to_i
+      end
+    end
+
+    # https://github.com/influxdata/influxdb-ruby/blob/master/lib/influxdb/timestamp_conversion.rb
+    FACTORS = {
+      "n"  => 1e9.to_r,
+      "u"  => 1e6.to_r,
+      "ms" => 1e3.to_r,
+      "s"  => 1.to_r,
+      "m"  => 1.to_r / 60,
+      "h"  => 1.to_r / 60 / 60,
+    }.freeze
+
+    # https://github.com/influxdata/influxdb-ruby/blob/master/lib/influxdb/timestamp_conversion.rb
+    CLOCK_NAMES = {
+      "n"  => [:nanosecond, 1],
+      "u"  => [:microsecond, 1],
+      "ms" => [:millisecond, 1],
+      "s"  => [:second, 1],
+      "m"  => [:second, 60.to_r],
+      "h"  => [:second, (60 * 60).to_r],
+    }.freeze
+
+    FACTORS.keys == CLOCK_NAMES.keys or raise
+  end
+end
---
+++ test.rb
@@ -0,0 +1,64 @@
+require 'minitest/autorun'
+require_relative 'ilp'
+
+class ILP
+
+class TimeConvTest < MiniTest::Test
+  def test_precision
+    err = assert_raises ArgumentError do
+      TimeConv.new 'xx'
+    end
+    assert_match /invalid time prec/, err.message
+
+    now = Time.now
+    conv = TimeConv.new 's'
+    assert_equal now.to_i, conv.now.to_i
+
+    now = Time.now
+    conv = TimeConv.new 'n'
+    assert_in_delta now.to_f * 1e9, conv.now.to_i, 0.0001 * 1e9
+
+    now = Time.now
+    conv = TimeConv.new 'ms'
+    assert_equal (now.to_f * 1e3).to_i, conv.conv(now).to_i
+    assert_equal (now.to_f * 1e3).to_i, conv.coerce(now).to_i
+    assert_equal (now.to_f * 1e3).to_i, conv.coerce(conv.coerce(now)).to_i
+  end
+end
+
+class PointTest < MiniTest::Test
+  def test_attrs
+    err = assert_raises ArgumentError do
+      Point.new
+    end
+    assert_match /missing.+:series/, err.message
+
+    Point.new series: "aa", tags: {}, values: {}
+  end
+
+  def test_to_s
+    pt = Point.new series: "aa", tags: {}, values: {}
+    assert_raises EmptyValuesError do
+      pt.to_s
+    end
+
+    pt = Point.new series: "aa", tags: {}, values: {bb: 1}
+    assert_equal %{aa bb=1i}, pt.to_s
+
+    pt = Point.new series: "a a", tags: {}, values: {bb: 1}
+    assert_equal %{a\\ a bb=1i}, pt.to_s
+
+    pt = Point.new series: "a a", tags: {}, values: {bb: "xx"}
+    assert_equal %{a\\ a bb="xx"}, pt.to_s
+
+    pt = Point.new series: "a", tags: {cc: "dd"}, values: {bb: 1.0}
+    assert_equal %{a,cc=dd bb=1.0}, pt.to_s
+
+    pt = Class.new(Point).
+      tap { _1.time_conv = TimeConv.new "ms" }.
+      new series: "a", tags: {}, values: {bb: 1.0}, timestamp: Time.at(1234)
+    assert_equal %{a bb=1.0 1234000}, pt.to_s
+  end
+end
+
+end
